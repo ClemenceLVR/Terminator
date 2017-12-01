a25934fc6afc7dc9354fb78455c1a52dd555252f
ClemenceL <clemence.lievre@gmail.com>
2017-12-01 15:38:22 +0100

    Creation des classes NameExtensions et SimpleOrgans

---
+++ ListOfOrgans.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+/** 
+ * Class list of organs 
+ * Gathers all the organs that have been found in the text
+ * 
+ * @author clemence
+ * @version 1/12/17
+ */
+public class ListOfOrgans {
+
+	private ArrayList<Organs> organsList;
+	private ArrayList<Organs> organsFoundList;
+	private String nameList;
+	
+	/** 
+	 * Constructor of the class ListOfOrgans
+	 */
+	public ListOfOrgans () {
+		
+	}
+	
+	public ArrayList getOrgansList() {
+		return organsList;
+	}
+	
+	public void setOrgansList() {
+		
+	}
+	
+	public String getName() {
+		return nameList;
+	}
+	
+	public ArrayList getOrgansFoundList () {
+		return organsFoundList;
+	}
+	
+	public void setOrgansFoundList () {
+		
+	}
+}
---
+++ NameExtensions.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+/** 
+ * Name Extensions class
+ * (définition des names extensions ici ?)
+ * @author clemence
+ * @version 1/12/17
+ */
+public class NameExtensions {
+	private ArrayList<String> nameExtensionsList; 
+	
+	/**
+	 * Constructor of the class NameExtensions
+	 */
+	public NameExtensions() {
+		
+	}
+	
+	/**
+	 * This method allows the users to get the list of name extensions
+	 * @return nameExtensionsList (list of name extensions)
+	 */
+	public ArrayList getNameExtList() {
+		return nameExtensionsList;
+	}
+	
+	/**
+	 * This method allows the users to 
+	 */
+	public void setNameExtList() {
+		
+	}
+}
